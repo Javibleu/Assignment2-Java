5df59c6e9fc4b77bbaa7679a3ada4bd56554a649
Javier <nievasj@sheridancollege.ca>
2019-02-03 16:06:18 -0500

    Final Version before Format

--- src/nievasj/Banking.java
+++ src/nievasj/Banking.java
@@ -17,8 +17,6 @@ import java.util.Scanner;
  */
 public class Banking {
 
-    //static double depositInput;     //Keeps the value entered to be processed
-    //static double withdrawInput;    //Keeps the value entered to be processed
     /**
      * Main Method: create Objects and Call Methods to Validate and process
      * transactions
@@ -50,36 +48,39 @@ public class Banking {
     public static void printHeaderTable(ChequingAccount a) {
         System.out.println("Customer: " + a.customerName);
         System.out.println("===========================================");
-        System.out.printf("%s %9s %9s %7s %9s\n", "AccNo", "Deposit", "WithDraw", "Intr.", "Newbal");
+        System.out.printf("%s %7s %9s %7s %9s\n", "AccNo.", "Deposit",
+                "Withdraw", "Intr.", "NewBal");
         System.out.println("===========================================");
     }
 
     /**
      * Call input and check if the number entered is negative or not a number.
+     *
      * @param operation string that will used to build prompt message
-     * @return the input value entered by operator after Validation
+     * @return the input value entered by operator after validation
      */
     public static double validInput(String operation) {
-        boolean cont;       //flag to keep into loop or get out of it
+        boolean cont = true;       //flag to keep into loop or get out of it
         double input = 0.0;
         do {
             try {
                 input = (promptClient("Enter amount you want to" + operation));
-                cont = false;       //to go out of loop
+                cont = false;       //with no exceptions trowed, go out of loop
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
-                cont = true;    //continue into the loop asking for correct data
             } catch (InputMismatchException f) {
                 System.out.println("Error: The value entered is not a number");
-                cont = true;    //continue into the loop asking for correct data
-
             }
-
         } while (cont);
         return input;
     }// end of validDeposit Method
-   
 
+    /**
+     * Print prompt on Screen and check value
+     *
+     * @param prompt to receive the String that will be printed on Screen
+     * @return double after check if it is no a negative number
+     */
     public static double promptClient(String prompt) {
         Scanner sc = new Scanner(System.in);
         System.out.println(prompt);
@@ -90,8 +91,14 @@ public class Banking {
             throw new IllegalArgumentException("Error: This is a negative "
                     + "number");
         }
-    }
+    }// Ends PromptClient method
 
+    /**
+      Method that executes transactions on Accounts
+     * @param account Object on with will be executed the transaction
+     * @param depositInput Amount of money that will be deposited
+     * @param withdrawInput Amount of money that will be withdraw
+     */
     public static void executeTransactions(Account account,
             double depositInput, double withdrawInput) {
         account.AccountDeposit(depositInput);
