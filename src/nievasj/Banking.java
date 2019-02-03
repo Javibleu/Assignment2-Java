cd7aaca81d0ba8f031852aceba7eb007feb55413
Javier <nievasj@sheridancollege.ca>
2019-02-03 11:55:42 -0500

    Version with method to execute transactions and print

--- src/nievasj/Banking.java
+++ src/nievasj/Banking.java
@@ -17,43 +17,44 @@ import java.util.Scanner;
  */
 public class Banking {
 
-    static double depositInput;
-    static double withdrawInput;
-
+    //static double depositInput;     //Keeps the value entered to be processed
+    //static double withdrawInput;    //Keeps the value entered to be processed
+    
     /**
+     * Main Method: create Objects and Call Methods to Validate and process 
+     * transactions
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+        
         ChequingAccount account101 = new ChequingAccount("101", 1000, "Carlos");
         SavingsAccount account201 = new SavingsAccount("201", 1000, "Carlos");
-        InvestmentAccount account301 = new InvestmentAccount("301", 1000, "Carlos");
+        InvestAccount account301 = new InvestAccount("301", 1000, "Carlos");
 
-        validDeposit();
-        validWithdraw();
-
-        printTitle(account101);
+        double depositInput = validDeposit();                 //Call Input and Validation Method
+        double depositWithdraw= validWithdraw();                //Call Input and Validation Method
+        printHeaderTable(account101);   //Print Client name and Table's Header
+        executeTransactions(account101,depositInput,depositWithdraw);//Process 
+        executeTransactions(account201,depositInput,depositWithdraw);//Process 
+        executeTransactions(account301,depositInput,depositWithdraw);//Process 
         
-        executeTransactions(account101);
-        executeTransactions(account201);
-        executeTransactions(account301);
-        
+    }// main end
 
-    }
-
-    public static void printTitle(ChequingAccount a) {
+    public static void printHeaderTable(ChequingAccount a) {
         System.out.println("Customer: " + a.customerName);
         System.out.println("===========================================");
-        System.out.printf("%s %9s %9s %7s %9s\n", "AccNo", "Deposit", "WithDraw", "Intr", "Newbal");
+        System.out.printf("%s %9s %9s %7s %9s\n", "AccNo", "Deposit", "WithDraw", "Intr.", "Newbal");
         System.out.println("===========================================");
     }
-
-    public static void validDeposit() {
+/**
+ * Call input and check if the number entered is negative or not a number.
+ */
+    public static double validDeposit() {
         boolean cont;       //flag to keep into loop or get out of it
+        double input= 0.0;
         do {
             try {
-                depositInput = (promptClient("Enter amount you want to deposit:"));
+               input = (promptClient("Enter amount you want to deposit:"));
                 cont = false;       //to go out of loop
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -65,14 +66,15 @@ public class Banking {
             }
 
         } while (cont);
-
+return input;
     }
 
-    public static void validWithdraw() {
+    public static double validWithdraw() {
         boolean cont;       //flag to keep into loop or get out of it
+        double input= 0.0;
         do {
             try {
-                withdrawInput = (promptClient("Enter amount you want to withdraw: "));
+                input = (promptClient("Enter amount you want to withdraw: "));
                 cont = false;       //to go out of loop
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -84,9 +86,10 @@ public class Banking {
             }
 
         } while (cont);
-
+return input;
     }
 
+    
     public static double promptClient(String prompt) {
         Scanner sc = new Scanner(System.in);
         System.out.println(prompt);
@@ -99,7 +102,8 @@ public class Banking {
         }
     }
 
-    public static void executeTransactions(Account account) {
+    public static void executeTransactions(Account account, 
+            double depositInput, double withdrawInput ) {
         account.AccountDeposit(depositInput);
         try {
             account.AccountWithdraw(withdrawInput);
--- src/nievasj/InvestAccount.java
+++ src/nievasj/InvestAccount.java
@@ -1,5 +1,5 @@
 /*
-Class name: InvestmentAccount.java  
+Class name: InvestAccount.java  
 Name:       Javier Nievas
 Date:       2-Feb-2019
  
@@ -11,12 +11,12 @@ package nievasj;
  *
  * @author Javier Nievas
  */
-public class InvestmentAccount extends SavingsAccount {
+public class InvestAccount extends SavingsAccount {
 
-    public InvestmentAccount() {
+    public InvestAccount() {
     }
 
-    public InvestmentAccount(String accountNumber, double accountBalance, String customerName) {
+    public InvestAccount(String accountNumber, double accountBalance, String customerName) {
         super(accountNumber, accountBalance, customerName);
         this.interestRate = 3;
 
