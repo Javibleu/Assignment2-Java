790aef0ffb85eb0bf1211c8cee59238c1ea34e58
Javier <nievasj@sheridancollege.ca>
2019-02-03 16:55:57 -0500

    Final Version After Format

--- src/nievasj/ChequingAccount.java
+++ src/nievasj/ChequingAccount.java
@@ -1,31 +1,38 @@
+package nievasj;
 /*
 Class name: ChequingAccount.java  
 Name:       Javier Nievas
 Date:       2-Feb-2019
  
-Description:  Description about the class 
- */
-package nievasj;
-
-/**
- *
- * @author Javier Nievas
+Description: Investmend Account is a subclass of Savings Account with a 1% of 
+Interest Rate, this account can be Overdrafted up to $500.
  */
 public class ChequingAccount extends Account implements Overdraftable {
-
-    double overdraftAmount;
-    
+   double overdraftAmount;
+/**
+ * Default empty Constructor
+ */    
     public ChequingAccount() {
     }
-
+/**
+ * Constructor with Number, Balance and Customer Name arguments
+ * @param accountNumber Account number identification
+ * @param accountBalance Initial Balance of Account
+ * @param customerName Customer Name 
+ */
     public ChequingAccount(String accountNumber, double accountBalance, String customerName) {
         super(accountNumber, accountBalance, customerName);
         this.interestRate = 1;
         this.overdraftAmount = 500;
     }
-
+/**
+ * accountWithdraw Overrides Account abstract Method, this method do allows
+ * to overdraft up to $500 therefore; the balance of this account 
+ * can be negative. 
+ * @param amount receive the amount to be withdrawn
+ */
     @Override
-    public void AccountWithdraw(double amount) {
+    public void accountWithdraw(double amount) {
         if (amount > (accountBalance+overdraftAmount)) {
             throw new IllegalArgumentException("Overdraft exceeded. "
                     + "Transaction canceled");
@@ -33,8 +40,10 @@ public class ChequingAccount extends Account implements Overdraftable {
         withdrawAmount = amount; 
         this.accountBalance -= amount;
     }
-
- 
+/**
+ * Implements method from OverDraftable interface
+ * @param overdraftAmount receives the value to be updated
+ */
     @Override
     public void setOverdraftAmount(double overdraftAmount) {
         this.overdraftAmount = overdraftAmount;
